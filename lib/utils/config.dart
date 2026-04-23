611bef9886d11838a72421626774ee14334229e0
Balasubramanian <123bala2006@gmail.com>
2026-04-23 12:33:11 +0530

    pull

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,2 +1,3 @@
 //Backend connection
-const String baseUrl = "https://kz2nkt6c-3000.inc1.devtunnels.ms";
+// const String baseUrl = "https://kz2nkt6c-3000.inc1.devtunnels.ms";
+const String baseUrl = "https://ghj5w9n1-3000.inc1.devtunnels.ms";
